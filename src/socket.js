a869529a81d11ff768caa26e19ccfa0f725f7282
Hussain Abidi <hussainabidi2009@gmail.com>
2024-07-30 00:10:03 +0500

    binary messages

--- src/socket.js
+++ src/socket.js
@@ -7,6 +7,7 @@ class Socket {
 
   init() {
     this.ws = new WebSocket(this.addr);
+    this.ws.binaryType = "arraybuffer";
 
     let flag = false;
     let commands = [false];
@@ -26,7 +27,7 @@ class Socket {
           if (commands[i]) o += Math.pow(2, i);
         }
 
-        this.talk(0, global.movement, o);
+        this.talk(global.movement, o);
       },
 
       ready: () => flag
@@ -46,55 +47,56 @@ class Socket {
   }
 
   onmessage = (message) => {
-    let m = JSON.parse(message.data);
+    const view = new DataView(message.data);
 
-    //console.log("Message from server: ", m);
-    switch (m[0]) {
-      case 0:
-        global.index = m[1].id;
+    if (view.byteLength === 12) {
+      const id = view.getUint32(0, true);
+      const map = { width: view.getUint32(4, true), height: view.getUint32(8, true) };
 
-        global.map.serverData.width = m[1].map.width;
-        global.map.serverData.height = m[1].map.height;
+      global.index = id;
+      global.map.serverData.width = map.width;
+      global.map.serverData.height = map.height;
 
-        break;
-      case 1:
-        let ids = new Set();
-        for (let i = 1; i < m.length; i++) {
-          const id = m[i].id;
-          const pos = m[i].pos;
-          const size = m[i].size;
-
-          ids.add(id);
-
-          let entity = global.entities.find(e => e.index === id);
-          if (!entity) {
-            entity = { serverData: { x: 0, y: 0, size: 0 }, x: 0, y: 0, size: 0 };
-            global.entities.push(entity);
-          }
-
-          entity.index = id;
-          entity.serverData.x = pos.x;
-          entity.serverData.y = pos.y;
-          entity.serverData.size = size;
-
-          if (id === global.index) {
-            global.player.x = pos.x;
-            global.player.y = pos.y;
-            global.player.size = size;
-          }
-        }
-        for (let e of global.entities) {
-          if (!ids.has(e.index)) e.serverData.size = 0;
-        }
-
-        global.entities = global.entities.filter(e => {
-          if (!ids.has(e.index)) {
-            e.serverData.size = 0;
-          }
-          return ids.has(e.index) || e.serverData.size !== 0 || e.size !== 0;
-        });
-        break;
+      return;
     }
+
+    const ids = new Set();
+    const entitySize = 24;
+
+    for (let offset = 0; offset < view.byteLength; offset += entitySize) {
+      const pos = { x: view.getFloat64(offset, true), y: view.getFloat64(offset + 8, true) };
+      const size = view.getFloat32(offset + 16, true);
+      const id = view.getUint32(offset + 20, true);
+
+      ids.add(id);
+      let entity = global.entities.find(e => e.index === id);
+      if (!entity) {
+        entity = { serverData: { x: 0, y: 0, size: 0 }, x: 0, y: 0, size: 0 };
+        global.entities.push(entity);
+      }
+
+      entity.index = id;
+      entity.serverData.x = pos.x;
+      entity.serverData.y = pos.y;
+      entity.serverData.size = size;
+
+      if (id === global.index) {
+        global.player.x = pos.x;
+        global.player.y = pos.y;
+        global.player.size = size;
+      }
+    }
+
+    for (let e of global.entities) {
+      if (!ids.has(e.index)) e.serverData.size = 0;
+    }
+
+    global.entities = global.entities.filter(e => {
+      if (!ids.has(e.index)) {
+        e.serverData.size = 0;
+      }
+      return ids.has(e.index) || e.serverData.size !== 0 || e.size !== 0;
+    });
   }
 
   onclose = () => {
@@ -105,8 +107,14 @@ class Socket {
     global.gameStart = false;
   }
 
-  talk(...message) {
-    this.ws.send(JSON.stringify(message));
+  talk(movement, flags) {
+    const buffer = new ArrayBuffer(8); // 4 bytes for movement, 4 bytes for flags
+    const view = new DataView(buffer);
+
+    view.setFloat32(0, movement, true);
+    view.setUint32(4, flags, true);
+
+    this.ws.send(buffer);
   }
 }
 
