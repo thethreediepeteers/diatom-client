685cf27b95c4a3e62edad0af87a701de46d4b24f
0xffabc <139798156+0xffabc@users.noreply.github.com>
2024-12-03 17:40:53 +0500

    [Refactor | Optimise] Tried to refactor socket manager and fix memleaks

--- src/socket.js
+++ src/socket.js
@@ -4,59 +4,38 @@ class Socket {
   constructor(addr) {
     this.addr = addr;
     this.canvas = false;
+    this.commands = [false, false, false];
+    this.flag = false;
+    this.ws = null;
   }
 
-  init = (canvas) => {
+  init(canvas) {
     this.ws = new WebSocket(this.addr);
     this.ws.binaryType = "arraybuffer";
     this.canvas = canvas;
 
-    let flag = false;
-    let commands = [
-      false, // moving
-      false, // lmb
-      false  // rmb 
-    ];
-
-    this.cmd = {
-      set: (index, value) => {
-        if (commands[index] !== value || index === 0) {
-          commands[index] = value;
-          flag = true;
-        }
-      },
-
-      talk: () => {
-        flag = false;
-        let o = 0;
-
-        for (let i = 0; i < commands.length; i++) {
-          if (commands[i]) o |= (1 << i);
-        }
-
-        this.talk(global.movement, o);
-      },
-
-      ready: () => flag
-    }
-
-    this.ws.onopen = this.onopen;
-    this.ws.onmessage = this.onmessage;
-    this.ws.onclose = this.onclose;
+    this.ws.onopen = this.onOpen.bind(this);
+    this.ws.onmessage = this.onMessage.bind(this);
+    this.ws.onclose = this.onClose.bind(this);
   }
 
-  onopen = () => {
+  cmd(index, value) {
+    if (this.commands[index] !== value || index === 0) {
+      this.commands[index] = value;
+      this.flag = true;
+    }
+  }
+
+  talk() {
+    if (!this.flag) return;
+    const flags = this.commands.reduce((o, cmd, i) => o | (cmd ? (1 << i) : 0), 0);
+    this.sendData("m", global.movement, flags);
+  }
+
+  onOpen() {
     console.log("Connected to server.");
-
-    this.ws.commandCycle = setInterval(() => {
-      if (this.cmd.ready()) this.cmd.talk();
-    });
-
-    let buffer = new ArrayBuffer(1);
-    let view = new DataView(buffer);
-
-    view.setInt8(0, "s".charCodeAt(0), true); // spawn header
-    this.ws.send(buffer);
+    this.ws.commandCycle = setInterval(() => this.talk(), 100);
+    this.sendData("s", 0);
 
     global.gameStart = true;
     if (!this.canvas) {
@@ -66,157 +45,112 @@ class Socket {
     }
   }
 
-  onmessage = (message) => {
-    const view = new DataView(message.data);
-
-    let o = 0;
-    const messageType = String.fromCharCode(view.getUint8(o++, true));
-
-    switch (messageType) {
-      case 's': // spawn data
-        const id = view.getUint32(o, true);
-        global.index = id;
-        break;
-
-      case 'm': // map data
-        const map = { width: view.getUint32(o, true), height: view.getUint32(o + 4, true) };
-
-        global.map.serverData.width = map.width;
-        global.map.serverData.height = map.height;
-        break;
-
-      case 'u': // update message
-        const ids = new Set();
-
-        const pos = { x: 0, y: 0 };
-        const color = { r: 0, g: 0, b: 0 };
-
-        for (let offset = o; offset < view.byteLength;) {
-          pos.x = view.getFloat64(offset, true);
-          offset += 8;
-          pos.y = view.getFloat64(offset, true);
-          offset += 8;
-          const size = view.getFloat32(offset, true);
-          offset += 4;
-          const angle = view.getFloat32(offset, true);
-          offset += 4;
-          const id = view.getInt32(offset, true);
-          offset += 4;
-          const mockupId = view.getInt32(offset, true);
-          offset += 4;
-          const health = view.getInt32(offset, true);
-          offset += 4;
-          const maxHealth = view.getInt32(offset, true);
-          offset += 4;
-          const team = view.getInt32(offset, true);
-          offset += 4;
-          const shape = view.getUint8(offset, true);
-          offset += 1;
-          color.r = view.getUint8(offset, true);
-          offset += 1;
-          color.g = view.getUint8(offset, true);
-          offset += 1;
-          color.b = view.getUint8(offset, true);
-          offset += 1;
-
-          const colorStr = `#${color.r.toString(16).padStart(2, '0')
-            }${color.g.toString(16).padStart(2, '0')
-            }${color.b.toString(16).padStart(2, '0')
-            }`;
-
-          ids.add(id);
-
-          let entity = global.entities.get(id);
-          if (!entity) {
-            entity = {
-              serverData: {
-                x: 0,
-                y: 0,
-                angle: 0,
-                health: 0,
-                maxHealth: 0
-              },
-              x: 0,
-              y: 0,
-              size: 0,
-              angle: 0,
-              health: 0,
-              maxHealth: 0,
-              color: colorStr,
-              team: 0,
-              dead: false,
-            };
-            global.entities.set(id, entity);
-          }
-
-          entity.index = id;
-          entity.dt = 0;
-          entity.xOld = entity.x || entity.serverData.x;
-          entity.yOld = entity.y || entity.serverData.y;
-          entity.serverData.x = pos.x;
-          entity.serverData.y = pos.y;
-          entity.serverData.angle = angle;
-          entity.serverData.health = health;
-          entity.serverData.maxHealth = maxHealth;
-          entity.size = size;
-          entity.color = colorStr;
-          entity.shape = shape;
-          entity.team = team;
-          entity.mockupId = mockupId;
-
-          if (id === global.index) {
-            global.player.dt = 0;
-
-            global.player.xOld = global.player.x;
-            global.player.yOld = global.player.y;
-
-            global.player.serverX = pos.x;
-            global.player.serverY = pos.y;
-
-            global.player.size = size;
-            global.player.angle = angle;
-            global.player.color = colorStr;
-            global.player.shape = shape;
-            global.player.health = health;
-            global.player.maxHealth = maxHealth;
-            global.player.team = team;
-            global.player.mockupId = mockupId;
-          }
-        }
-
-        for (let [id, entity] of global.entities) {
-          if (!ids.has(id)) {
-            entity.dead = true;
-          }
-        }
-
-        global.entities = new Map([...global.entities].filter(([id, entity]) => {
-          return ids.has(id) || !entity.dead || id === global.index;
-        }));
-
-        break;
-    }
-  }
-
-  onclose = () => {
-    console.log("Connection closed.");
-    clearInterval(this.ws.commandCycle);
-
-    if (global.gameStart) global.disconnected = true;
-    global.gameStart = false;
-  }
-
-  talk = (movement, flags) => {
-    const buffer = new ArrayBuffer(13); // 1 byte for header, 4 bytes for mouse, 4 bytes for movement, 4 bytes for flags
+  sendData(header, movement, flags) {
+    const buffer = new ArrayBuffer(header === "s" ? 1 : 13);
     const view = new DataView(buffer);
-
-    view.setInt8(0, "m".charCodeAt(0), true); // update header
+    
+    view.setInt8(0, header.charCodeAt(0));
+    if (header === "s") {
+      this.ws.send(buffer);
+      return;
+    }
     view.setInt16(1, global.target.x, true);
     view.setInt16(3, global.target.y, true);
     view.setFloat32(5, movement, true);
     view.setUint32(9, flags, true);
-
     this.ws.send(buffer);
+  }
+
+  onMessage(message) {
+    const view = new DataView(message.data);
+    const messageType = String.fromCharCode(view.getUint8(0));
+
+    switch (messageType) {
+      case 's':
+        global.index = view.getUint32(1, true);
+        break;
+      case 'm':
+        const map = { width: view.getUint32(1, true), height: view.getUint32(5, true) };
+        Object.assign(global.map.serverData, map);
+        break;
+      case 'u':
+        this.updateEntities(view);
+        break;
+    }
+  }
+
+  updateEntities(view) {
+    const ids = new Set();
+    let offset = 1;
+
+    while (offset < view.byteLength) {
+      const entityData = this.extractEntityData(view, offset);
+      const entity = global.entities.get(entityData.id) || this.createEntity(entityData.id);
+      this.updateEntity(entity, entityData);
+      ids.add(entityData.id);
+      offset += entityData.sizeBytes;
+    }
+
+    this.removeDeadEntities(ids);
+  }
+
+  extractEntityData(view, offset) {
+    const pos = { x: view.getFloat64(offset, true), y: view.getFloat64(offset + 8, true) };
+    const entityData = {
+      size: view.getFloat32(offset + 16, true),
+      angle: view.getFloat32(offset + 20, true),
+      id: view.getInt32(offset + 24, true),
+      mockupId: view.getInt32(offset + 28, true),
+      health: view.getInt32(offset + 32, true),
+      maxHealth: view.getInt32(offset + 36, true),
+      team: view.getInt32(offset + 40, true),
+      shape: view.getUint8(offset + 44, true),
+      color: this.getColor(view, offset + 45),
+    };
+    return { ...entityData, pos, sizeBytes: 49 };
+  }
+
+  getColor(view, offset) {
+    const r = view.getUint8(offset, true);
+    const g = view.getUint8(offset + 1, true);
+    const b = view.getUint8(offset + 2, true);
+    return `#${r.toString(16).padStart(2, '0')}${g.toString(16).padStart(2, '0')}${b.toString(16).padStart(2, '0')}`;
+  }
+
+  createEntity(id) {
+    const entity = {
+      serverData: { x: 0, y: 0, angle: 0, health: 0, maxHealth: 0 },
+      x: 0, y: 0, size: 0, angle: 0, health: 0, maxHealth: 0, color: "", team: 0, dead: false
+    };
+    global.entities.set(id, entity);
+    return entity;
+  }
+
+  updateEntity(entity, { id, pos, size, angle, health, maxHealth, color, team, mockupId }) {
+    entity.index = id;
+    entity.xOld = entity.x || entity.serverData.x;
+    entity.yOld = entity.y || entity.serverData.y;
+    Object.assign(entity.serverData, { x: pos.x, y: pos.y, angle, health, maxHealth });
+    Object.assign(entity, { size, color, team, mockupId });
+    if (id === global.index) {
+      Object.assign(global.player, { serverX: pos.x, serverY: pos.y, size, angle, color, team, mockupId });
+    }
+  }
+
+  removeDeadEntities(ids) {
+    for (const [id, entity] of global.entities) {
+      if (!ids.has(id)) {
+        entity.dead = true;
+      }
+    }
+    global.entities = new Map([...global.entities].filter(([id, entity]) => ids.has(id) || !entity.dead || id === global.index));
+  }
+
+  onClose() {
+    console.log("Connection closed.");
+    clearInterval(this.ws.commandCycle);
+    global.gameStart = false;
+    global.disconnected = true;
   }
 }
 
