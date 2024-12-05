63019555634f1ec227acb5aa0ee70bf9a13117bd
DenisC <denis0ches00@gmail.com>
2024-12-05 19:10:30 +0700

    [dev] more types

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,37 @@
+interface ServerEntity {
+    x: number;
+    y: number;
+    health: number;
+    maxHealth: number;
+    angle: number;
+}
+
+interface Entity extends ServerEntity {
+    index: number;
+    mockupId: number;
+    xOld: number;
+    yOld: number;
+    size: number;
+    shape: number;
+    team: number;
+    color: HexColor; // NOTE: its not cached in socket.js
+    serverData: ServerEntity;
+    dead: boolean;
+    dt: number;
+}
+
+interface Mockup {
+    guns: any[],
+    turrets: any[],
+    // TODO
+}
+
+type ColorStrings = 'col1' | 'col2' | 'col3' | 'col4' | 'col5' | 'col6' | 'col7' | 'col8';
+type HexColor = `#${string}`;
+
 interface DiatomGlobal {
     movement: number;
-    entities: Map<number, any>;
+    entities: Map<number, Entity>;
     gameStart: boolean;
     disconnected: boolean;
     map: {
@@ -8,21 +39,23 @@ interface DiatomGlobal {
         height: number,
         serverData: { width: number, height: number }
     };
-    player: {
-        x: number,
-        y: number,
-        size: number,
-        shape: number,
-        angle: number,
-        color: string,
-        mockupId: number
-    };
+    player: Omit<Entity, 'index' | 'serverData'> | { serverX: number, serverY: number };
     mouse: { x: number, y: number };
     target: { x: number, y: number };
-    mockups: Map<number, any>;
-    colors: Map<string, string>;
-    borderColorsCache: Map<number, string>;
-    color: string
+    mockups: Map<number, Mockup>;
+    colors: Map<ColorStrings, HexColor>;
+    borderColorsCache: Map<HexColor, HexColor>;
+    color: HexColor, // should be in player
+
+    // undefined in global by default
+    deltaTime: number,
+    index: number, // should be in player
+    mouseAngle: number,
+
+    //camX and camY are not defined in global
+    //camX: number,
+    //camY: number,
+
 }
 
 declare const global: DiatomGlobal;
